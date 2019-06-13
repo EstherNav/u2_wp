3872e5996a38c2ea89b24d3537eee8b8906a1040
EstherNav <al1609091@upy.edu.mx>
2019-06-13 11:14:13 -0500

    keyboard programmed

--- controllers/u2_wp/u2_wp.c
+++ controllers/u2_wp/u2_wp.c
@@ -1,20 +1,10 @@
-/*
- * File:          u2_wp.c
- * Date:
- * Description:
- * Author:
- * Modifications:
- */
-
-/*
- * You may need to add include files like <webots/distance_sensor.h> or
- * <webots/differential_wheels.h>, etc.
- */
 #include <webots/robot.h>
+#include <webots/motor.h>
+#include <webots/keyboard.h>
 
-/*
- * You may want to add macros here.
- */
+#include <stdio.h>
+
+
 #define TIME_STEP 64
 
 /*
@@ -26,7 +16,23 @@ int main(int argc, char **argv)
 {
   /* necessary to initialize webots stuff */
   wb_robot_init();
+    
+  wb_keyboard_enable(TIME_STEP);
+  int pressed_key;
+  
 
+   WbDeviceTag wheel_right = wb_robot_get_device("motor_right");
+   WbDeviceTag wheel_left = wb_robot_get_device("motor_left");
+   
+   
+   
+   
+   WbDeviceTag dis_sensor = wb_robot_get_device("distance_sensor");
+   wb_distance_sensor_enable(dis_sensor, TIME_STEP);
+   double ds_value;
+   
+   wb_motor_set_position(wheel_right, INFINITY);
+   wb_motor_set_position(wheel_left, INFINITY);
   /*
    * You should declare here WbDeviceTag variables for storing
    * robot devices like this:
@@ -40,14 +46,36 @@ int main(int argc, char **argv)
    */
   while (wb_robot_step(TIME_STEP) != -1) {
 
+
+
+     pressed_key = wb_keyboard_get_key();
     /*
      * Read the sensors :
      * Enter here functions to read sensor data, like:
      *  double val = wb_distance_sensor_get_value(my_sensor);
      */
-
-    /* Process sensor data here */
-
+ if (pressed_key == WB_KEYBOARD_UP){
+       wb_motor_set_velocity(wheel_right, -5);
+       wb_motor_set_velocity(wheel_left, -5);
+       
+     }
+       
+     if (pressed_key == WB_KEYBOARD_LEFT){
+       wb_motor_set_velocity(wheel_right, -5);
+       wb_motor_set_velocity(wheel_left, 5);
+     }
+     
+     if (pressed_key == WB_KEYBOARD_RIGHT){
+       wb_motor_set_velocity(wheel_right, 5);
+       wb_motor_set_velocity(wheel_left, -5);
+     }
+     
+     if (pressed_key == WB_KEYBOARD_DOWN){
+       wb_motor_set_velocity(wheel_right, 5);
+       wb_motor_set_velocity(wheel_left, 5);
+     }
+       
+  
     /*
      * Enter here functions to send actuator commands, like:
      * wb_differential_wheels_set_speed(100.0,100.0);
